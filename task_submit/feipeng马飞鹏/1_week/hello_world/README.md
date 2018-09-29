a895980277f4a7c1370946718ef923d843f4ade6
XIUXIUXIUBIUA <addtodi@sina.com>
2018-09-29 22:57:54 +0800

    readme

---
+++ task_submit/feipeng马飞鹏/1_week/hello_world/README.md
@@ -0,0 +1,19 @@
+## Some details about the hello world project
+- Author: 马飞鹏
+- Revised:马飞鹏
+- Date: 2018-9-29
+- Version: 1.0.0
+- Abstract: 主要是如何编译
+
+
+### Function
+  输出hello world！到终端
+
+### Depend Libraries
+  C++基本输入输出库： <iostream>
+
+### how to compile
+  cmake .
+  make
+### how to run
+ ./HelloWorld
--- task_submit/feipeng马飞鹏/1_week/programming_exercise/Pailndromes.cpp
+++ task_submit/feipeng马飞鹏/1_week/programming_exercise/Pailndromes.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
 #include<string> 
 using namespace std;
-const char change_before[]={'3','L','2','5'};
+const char change_before[]={'3','L','2','5'};	//将对称的词改成同一个
 const char change_after[]= {'E','J','S','Z'};
 const char every_cannotbe[] = {'B','C','D','F','G','K','N','P','Q','R','4','6','7','9'};
 const char mid_cannotbe[]={'E','3','J','L','S','2','Z','5','1'};
+/*
+ *思路：先判断是否为普通的对称字符串，再将原字符串中的对称的字母成同一个,再判断是否为对称字符串，
+ *注意此时还不能判断是否为镜像字符串，还要注意每一个字符都不能为非对称字符，然后如果字符串长度
+ *为奇数，则中间的字符必须自身对称。
+ * */
 void solve(string &k)
 {
-	string input = k;
-	int len = input.size();			
-	int times = len/2;	
+	string input = k;//input为被处理的字符串
+	int len = input.size();//len为字符串长度			
+	int times = len/2;	//tiems为字符串半长
 	bool regular,mirror;			
 	int i,j;
 	for(i = 0;i<times;i++)
