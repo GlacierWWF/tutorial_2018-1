52567c836cb12c093c23e25ffddfe69beaed3492
ubuntu <1129101290@qq.com>
2018-09-29 19:07:15 +0800

    1.0

---
+++ task_submit/曾向进/1_week/HelloWorld.cpp
@@ -0,0 +1,11 @@
+//Tittle:HelloWorld
+//Author:曾向进
+//Date:2018-9-28
+//Vision:1.0
+//Function:output "Hello World"
+#include<iostream>
+using namespace std;
+int main()
+{cout<<"Hello World"<<endl;
+return 0;
+}
