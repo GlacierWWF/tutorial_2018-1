6f02d4c8f6be0affb8ca5d4a3fdc241dc7e1d880
jackkii66 <chenjq66@mail2.sysu.edu.cn>
2018-09-29 17:50:22 +0800

    HelloWorld.cpp

---
+++ task_submit/jackkii陈洁琪/1_week/hello_world/hello_world.cpp
@@ -0,0 +1,36 @@
+/****************************************************************
+ * Hello World                                                  *
+ * @file    hello_world.cpp                                     *
+ * @brief   print Hello World!                                  *
+ *                                                              *
+ * @author  JackkiiChen                                         *
+ * @email   chenjq66@mail2.sysu.edu.cn                          *
+ * @version 1.0							*
+ * @date    2018/09/29                                          *
+ *                                                              *
+ * ------------------------------------------------------------ *
+ *  Change History:                                             *
+ *  <Date>     | <Version>|  <Author>   | <Description>         *
+ *  ----------------------------------------------------------- *
+ *  2018/09/29 | 1.0      | JackkiiChen |  print Hello World!   *
+ *  ----------------------------------------------------------- *
+ ***************************************************************/
+
+
+//System Headers
+#include<iostream>
+
+//using keyword
+using std::cout;
+using std::endl;
+
+/**
+ *@brief Hello World Main Function
+ */
+int main()
+{
+	//print Hello World!
+	cout<<"Hello World!"<<endl;
+	return 0;
+}
+
