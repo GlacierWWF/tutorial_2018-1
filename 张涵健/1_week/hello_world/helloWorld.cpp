19a3357e382b90ca3d28772db49570bd54258a7e
CoderAT13 <969437072@qq.com>
2018-09-29 19:52:24 +0800

    hello world

---
+++ 张涵健/1_week/hello_world/helloWorld.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  
+//  Created by 张涵健 on 2018/9/21.
+//  Copyright © 2018年 张涵健Coder.@. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
