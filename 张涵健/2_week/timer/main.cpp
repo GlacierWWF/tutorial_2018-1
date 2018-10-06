f26b9c7de3217dca2fdae39f2522662f80aac41c
CoderAT13 <969437072@qq.com>
2018-10-06 19:17:12 +0800

    2_week2_week

---
+++ 张涵健/2_week/timer/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  timer
+//
+//  Created by 张涵健 on 2018/10/5.
+//  Copyright © 2018 张涵健Coder.@. All rights reserved.
+//
+
+#include <stdio.h>
+#include "timer_client.h"
+using namespace std;
+
+int main()
+{
+    Timer test;
+    test.time();
+
+    return 0;
+}
+
