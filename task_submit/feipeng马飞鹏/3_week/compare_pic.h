35a05aa09bb84beff59157f47a8726ddc4490950
XIUXIUXIUBIUA <addtodi@sina.com>
2018-10-14 23:08:07 +0800

    again

--- task_submit/feipeng马飞鹏/3_week/compare_pic.h
+++ task_submit/feipeng马飞鹏/3_week/compare_pic.h
@@ -1,5 +1,11 @@
 #ifndef _COMPARE_PIC_H
 #define _COMPARE_PIC_H
+#include<iostream>
+#include<opencv2/opencv.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<vector>
+using namespace cv;
+using namespace std;
 bool cmp(const Rect& a,const Rect &b);
 
 
