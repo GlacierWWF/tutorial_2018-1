84c64a614d05bedd747dc8967b8d7dedceb3d421
Atl <atlanswer@gmail.com>
2018-11-11 22:56:26 +0800

    Final update

--- task_submit/atlanswer赵鑫/project/Task 3/CNN_for_gesture.py
+++ task_submit/atlanswer赵鑫/project/Task 3/CNN_for_gesture.py
@@ -45,9 +45,7 @@ CONV_SIZE = 3
 training_images_path = './training_images'
 
 # weight files
-WeightFileName = ["ori_4015imgs_weights.hdf5", "bw_4015imgs_weights.hdf5", "bw_2510imgs_weights.hdf5",
-                  "./bw_weight.hdf5", "./final_c_weights.hdf5", "./semiVgg_1_weights.hdf5",
-                  "/new_wt_dropout20.hdf5", "./weights-CNN-gesture_skinmask.hdf5"]
+WeightFileName = ["saved_model\gesture_model.hdf5"]
 
 # outputs
 output = ["OK", "NOTHING", "PEACE", "PUNCH", "STOP"]
